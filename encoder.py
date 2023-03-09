25eec32e16b299287a1734427e0c94ad9597f620
Muhammad Ali <sir.ali.of.aether@gmail.com>
2023-03-08 21:53:59 -0500

    -m "First Commit"

--- encoder.py
+++ encoder.py
@@ -1,8 +1,32 @@
-#Hey
-print('Hello World')
+# Brandon Scheiber & Muhammad Ali
+# COP3502
+
+def prompt(): 
+    #prints menu for the user
+    print("Menu\n\n------------\n\n1. Encode\n\n2. Decode\n\n3. Quit\n")
+
+    user_input = input("Please enter an option: ")
+    return user_input
+
+def encoder():
+    pass
+
+
+def decoder():
+    pass
 
 
 def main():
-	pass
+    while True:
+        user_input = prompt()
+        if user_input == "1":
+            n_password = encoder()
+            # print(str(n_password))  #For testing
+        elif user_input == "2":
+            decoder(n_password)
+        else: #quit
+            break
 
 
+if __name__ == '__main__':
+    main()
